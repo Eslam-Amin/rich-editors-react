0fe7917b3ad2509a2b105db094b6ac70aa8f3b9e
Eslam Amin <eslam.amin.610@gmail.com>
2025-08-31 21:55:18 +0300

    added comparison route

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ export default function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<Navigate to="/comparison" replace />} />
+          <Route path="/comparison" element={<Navigate to="/comparison" />} />
           <Route path="/quill" element={<QuillEditor />} />
           <Route path="/slate" element={<SlateEditor />} />
           <Route path="/lexical" element={<LexicalEditor />} />
